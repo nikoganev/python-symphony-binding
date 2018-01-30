90c19c2174015b9651708519175e7b26df76df03
openfly <matt@nycresistor.com>
2018-01-30 10:55:48 -0500

    grrr

--- setup.py
+++ setup.py
@@ -51,5 +51,5 @@ setup(
     include_package_data=True,
     data_files=[('config', ['swagger-specs/agent-api-public-deprecated.yaml',
                             'swagger-specs/pod-api-public-deprecated.yaml'])],
-    zip_safe=True
+    zip_safe=False
 )
